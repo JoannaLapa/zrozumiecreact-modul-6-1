7c5448ad995f74ef4fe67b62fec3509240a4bae3
Joanna <jlapa.transport@gmail.com>
2024-01-25 08:08:55 +0100

    active props added

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,14 +1,14 @@
 import styles from "./List.module.css";
 import { Button } from "../Button/Button";
 
-export function List({ recipes, onSelectRecipe }) {
+export function List({ selectedRecipe, recipes, onSelectRecipe }) {
     return (
         <nav className={styles.nav}>
             <span className={styles.title}>Lista przepisów:</span>
             <ul className={styles.list}>
                 {recipes.map(({ id, name }) => (
                     <li key={id} className={styles.item}>
-                        <Button onClick={() => onSelectRecipe(id)}>
+                        <Button active={ id === selectedRecipe.id } onClick={() => onSelectRecipe(id)}>
                             {name}
                         </Button>
                     </li>
